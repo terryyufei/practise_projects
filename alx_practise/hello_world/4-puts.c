aac97c3a5460014eb88b2d2b0d03031ac146b490
Teresia WAmbui <shikuwambui@gmail.com>
2023-03-17 20:27:54 +0300

    hello world

---
+++ alx_practise/hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - a function that uses puts
+  *
+  *Return: 0
+  */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
